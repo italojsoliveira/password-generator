796a89e3a1e02806430dd9a53815526bf06c95d3
Ítalo Oliveira <italojsoliveira1@gmail.com>
2024-10-11 01:23:35 +0200

    app

---
+++ password_generator.py
@@ -0,0 +1,53 @@
+import random
+import string
+import streamlit as st
+
+st.title('Password Generator')
+
+if st.checkbox('Do you want to have numbers?'):
+    has_number = True
+else:
+    has_number = False
+
+if st.checkbox('Do you want to have special characters?'):
+    has_special = True
+else:
+    has_special = False
+
+def generate_password(min_length, has_number, has_special):
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+
+    characters = letters
+    if has_number:
+        characters += digits
+    if has_special:
+        characters += special
+    
+    pwd = ""
+
+    # at least one number if so chosen
+    if has_number:
+        new_number = random.choice(digits)
+        pwd += new_number
+
+    # at least one special character if so chosen
+    if has_special:
+        new_special = random.choice(special)
+        pwd += new_special
+
+
+    while len(pwd) < min_length:
+        new_char = random.choice(characters)
+        pwd += new_char
+
+    return pwd
+
+
+min_length = st.slider('Minimal Length', 12, 60, 20)  # min, max, default
+
+pwd = generate_password(min_length, has_number, has_special)
+
+st.subheader('Password')
+st.write(pwd)
